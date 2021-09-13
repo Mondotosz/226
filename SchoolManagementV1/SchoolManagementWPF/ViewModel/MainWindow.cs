b013dcfce72703c332d257dde800c750a6892345
Mon <55015209+Mondotosz@users.noreply.github.com>
2021-09-13 15:55:01 +0200

    wpf

---
+++ SchoolManagementV1/SchoolManagementWPF/ViewModel/MainWindow.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Toolkit.Mvvm;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using System.Windows.Input;
+using System.Windows;
+
+namespace SchoolManagementWPF.ViewModel
+{
+    public class MainWindow : ObservableObject
+    {
+        private WindowState _currentWindowState;
+        public WindowState CurrentWindowState
+        {
+            get => _currentWindowState;
+            set
+            {
+                _currentWindowState = value;
+                base.OnPropertyChanged("CurrentWindowState");
+            }
+        }
+        public MainWindow()
+        {
+
+        }
+
+        public ICommand BtnMaximize
+        {
+            get { return new RelayCommand<object>(param => onBtnMaximize()); }
+        }
+        public ICommand BtnMinimize
+        {
+            get { return new RelayCommand<object>(param => onBtnMinimize()); }
+        }
+        public ICommand BtnClose
+        {
+            get { return new RelayCommand<object>(param => onBtnClose()); }
+        }
+
+        void onBtnMaximize()
+        {
+            CurrentWindowState = CurrentWindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+        void onBtnMinimize()
+        {
+            CurrentWindowState = WindowState.Minimized;
+        }
+        void onBtnClose()
+        {
+            
+        }
+    }
+}
