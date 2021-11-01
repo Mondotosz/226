0402f0c135599d2115d507031772574162de19d4
Mon <55015209+Mondotosz@users.noreply.github.com>
2021-11-01 11:33:45 +0100

    formative 2

---
+++ Formative/226_com_tdd_start/FlightStats/Measure.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightStats
+{
+    public class Measure
+    {
+        private double _temperatureInFahrenheit;
+        private double _temperatureInCelsius;
+        private double _temperatureInKelvin;
+        private double _localSpeedOfSound;
+
+        public Measure(double temperatureInFahrenheit)
+        {
+            _temperatureInFahrenheit = temperatureInFahrenheit;
+        }
+
+        public Measure(double temperatureInFahrenheit, double temperatureInCelsius)
+        {
+            _temperatureInFahrenheit = temperatureInFahrenheit;
+            _temperatureInCelsius = temperatureInCelsius;
+        }
+
+        public Measure(double temperatureInFahrenheit, double temperatureInCelsius, double temperatureInKelvin)
+        {
+            _temperatureInFahrenheit = temperatureInFahrenheit;
+            _temperatureInCelsius = temperatureInCelsius;
+            _temperatureInKelvin = temperatureInKelvin;
+        }
+
+        public Measure(double temperatureInFahrenheit, double temperatureInCelsius, double temperatureInKelvin, double localSpeedOfSound)
+        {
+            _temperatureInFahrenheit = temperatureInFahrenheit;
+            _temperatureInCelsius = temperatureInCelsius;
+            _temperatureInKelvin = temperatureInKelvin;
+            _localSpeedOfSound = localSpeedOfSound;
+        }
+        public override string ToString()
+        {
+
+            AeroConverter aeroConverter = new AeroConverter();
+            _temperatureInCelsius =  aeroConverter.ConvertFahrenheitToCelsius(_temperatureInFahrenheit);
+            _temperatureInKelvin =  aeroConverter.ConvertFahrenheitToKelvin(_temperatureInFahrenheit);
+            _localSpeedOfSound =  aeroConverter.LocalSpeedOfSound(_temperatureInKelvin);
+            return $"{_temperatureInFahrenheit.ToString("0.00")};{_temperatureInCelsius.ToString("0.00")};{_temperatureInKelvin.ToString("0.00")};{_localSpeedOfSound.ToString("0.00")}";
+        }
+    }
+}
